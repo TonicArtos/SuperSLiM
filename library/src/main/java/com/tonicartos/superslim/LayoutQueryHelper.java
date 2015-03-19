9d7d31cc9632127087a9af2042471f6c391e10a4
Tonic Artos <tonic.artos@gmail.com>
2015-03-19 03:01:46 +0000

    Removed BaseLayoutQueryHelper for now.
    Still thinking about it.

--- library/src/main/java/com/tonicartos/superslim/LayoutQueryHelper.java
+++ library/src/main/java/com/tonicartos/superslim/LayoutQueryHelper.java
@@ -1,6 +1,37 @@
 package com.tonicartos.superslim;
 
-public interface LayoutQueryHelper extends BaseLayoutQueryHelper {
+import android.view.View;
+
+interface LayoutQueryHelper {
 
     int getStickyEdge();
+
+    int getBottom(View child);
+
+    View getChildAt(int index);
+
+    int getChildCount();
+
+    int getHeight();
+
+    int getLayoutDirection();
+
+    int getLeft(View child);
+
+    int getMeasuredHeight(View v);
+
+    int getMeasuredWidth(View v);
+
+    int getPosition(View child);
+
+    int getRight(View child);
+
+    SectionData getSectionData(int position);
+
+    SectionLayoutManager getSlm(SectionData sectionData, LayoutQueryHelper helper);
+
+    int getTop(View child);
+
+    int getWidth();
+
 }
