7e9b3224640ec654940f9288c9143af4b2eb7061
Tonic Artos <tonic.artos@gmail.com>
2015-03-21 04:52:32 +0000

    Update example for digraph changes.

--- example/src/main/java/com/tonicartos/superslimexample/CountryNamesAdapter.java
+++ example/src/main/java/com/tonicartos/superslimexample/CountryNamesAdapter.java
@@ -1,7 +1,9 @@
 package com.tonicartos.superslimexample;
 
 import com.tonicartos.superslim.GridSLM;
+import com.tonicartos.superslim.LayoutManager;
 import com.tonicartos.superslim.LinearSLM;
+import com.tonicartos.superslim.SectionAdapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
@@ -11,11 +13,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  */
-public class CountryNamesAdapter extends RecyclerView.Adapter<CountryViewHolder> {
+public class CountryNamesAdapter extends RecyclerView.Adapter<CountryViewHolder> implements SectionAdapter<CountryNamesAdapter.Section> {
 
     private static final int VIEW_TYPE_HEADER = 0x01;
 
@@ -23,7 +26,7 @@ public class CountryNamesAdapter extends RecyclerView.Adapter<CountryViewHolder>
 
     private static final int LINEAR = 0;
 
-    private final ArrayList<LineItem> mItems;
+    private final Section mSectionGraph = new Section();
 
     private int mHeaderDisplay;
 
@@ -37,33 +40,43 @@ public class CountryNamesAdapter extends RecyclerView.Adapter<CountryViewHolder>
         final String[] countryNames = context.getResources().getStringArray(R.array.country_names);
         mHeaderDisplay = headerMode;
 
-        mItems = new ArrayList<>();
-
         //Insert headers into list of items.
-        String lastHeader = "";
+        String lastHeaderText = "";
         int sectionManager = -1;
         int headerCount = 0;
         int sectionFirstPosition = 0;
+        Section currentSection = new Section();
         for (int i = 0; i < countryNames.length; i++) {
-            String header = countryNames[i].substring(0, 1);
-            if (!TextUtils.equals(lastHeader, header)) {
+            String headerText = countryNames[i].substring(0, 1);
+            if (!TextUtils.equals(lastHeaderText, headerText)) {
                 // Insert new header view and update section data.
                 sectionManager = (sectionManager + 1) % 2;
                 sectionFirstPosition = i + headerCount;
-                lastHeader = header;
+                lastHeaderText = headerText;
                 headerCount += 1;
-                mItems.add(new LineItem(header, true, sectionManager, sectionFirstPosition));
+                currentSection.setEnd(sectionFirstPosition - 1);
+                currentSection = new Section(sectionFirstPosition);
+                currentSection.addHeader(
+                        new LineItem(headerText, true, sectionManager, sectionFirstPosition));
+                mSectionGraph.addSubsection(currentSection);
             }
-            mItems.add(new LineItem(countryNames[i], false, sectionManager, sectionFirstPosition));
+            currentSection.addItem(
+                    new LineItem(countryNames[i], false, sectionManager, sectionFirstPosition));
         }
+        currentSection.setEnd(mSectionGraph.getCount());
+    }
+
+    @Override
+    public List<Section> getSections() {
+        return mSectionGraph.getSubsections();
     }
 
     public boolean isItemHeader(int position) {
-        return mItems.get(position).isHeader;
+        return mSectionGraph.getItem(position).isHeader;
     }
 
     public String itemToString(int position) {
-        return mItems.get(position).text;
+        return mSectionGraph.getItem(position).text;
     }
 
     @Override
@@ -81,7 +94,7 @@ public class CountryNamesAdapter extends RecyclerView.Adapter<CountryViewHolder>
 
     @Override
     public void onBindViewHolder(CountryViewHolder holder, int position) {
-        final LineItem item = mItems.get(position);
+        final LineItem item = mSectionGraph.getItem(position);
         final View itemView = holder.itemView;
 
         holder.bindItem(item.text);
@@ -100,23 +113,27 @@ public class CountryNamesAdapter extends RecyclerView.Adapter<CountryViewHolder>
 
         if (position == item.sectionFirstPosition) {
             lp.setSlm(item.sectionManager == LINEAR ? LinearSLM.ID : GridSLM.ID);
-            lp.marginEndIsAuto = !mMarginsFixed;
-            lp.marginStartIsAuto = !mMarginsFixed;
-            lp.setColumnWidth(mContext.getResources().getDimensionPixelSize(R.dimen.grid_column_width));
+            if (!mMarginsFixed) {
+                lp.marginEnd = LayoutManager.LayoutParams.MARGIN_AUTO;
+            }
+            if (!mMarginsFixed) {
+                lp.marginStart = LayoutManager.LayoutParams.MARGIN_AUTO;
+            }
+            lp.setColumnWidth(
+                    mContext.getResources().getDimensionPixelSize(R.dimen.grid_column_width));
         }
 
-        lp.setFirstPosition(item.sectionFirstPosition);
         itemView.setLayoutParams(lp);
     }
 
     @Override
     public int getItemViewType(int position) {
-        return mItems.get(position).isHeader ? VIEW_TYPE_HEADER : VIEW_TYPE_CONTENT;
+        return mSectionGraph.getItem(position).isHeader ? VIEW_TYPE_HEADER : VIEW_TYPE_CONTENT;
     }
 
     @Override
     public int getItemCount() {
-        return mItems.size();
+        return mSectionGraph.getCount();
     }
 
     public void setHeaderDisplay(int headerDisplay) {
@@ -130,8 +147,9 @@ public class CountryNamesAdapter extends RecyclerView.Adapter<CountryViewHolder>
     }
 
     private void notifyHeaderChanges() {
-        for (int i = 0; i < mItems.size(); i++) {
-            LineItem item = mItems.get(i);
+        final int count = mSectionGraph.getCount();
+        for (int i = 0; i < count; i++) {
+            final LineItem item = mSectionGraph.getItem(i);
             if (item.isHeader) {
                 notifyItemChanged(i);
             }
@@ -154,6 +172,70 @@ public class CountryNamesAdapter extends RecyclerView.Adapter<CountryViewHolder>
             this.text = text;
             this.sectionManager = sectionManager;
             this.sectionFirstPosition = sectionFirstPosition;
+        }
+    }
+
+    public static class Section extends SectionAdapter.Section<Section> {
+
+        private ArrayList<LineItem> mItems = new ArrayList<>();
+        private LineItem mHeader;
+
+        public Section() {
+        }
+
+        public Section(int start) {
+            super(start);
+        }
+
+        public Section addHeader(LineItem header) {
+            mHeader = header;
+            return this;
+        }
+
+        public Section addItem(LineItem item) {
+            mItems.add(item);
+            return this;
+        }
+
+        public Section addSubsection(Section section) {
+            if (subsections == null) {
+                subsections = new ArrayList<>();
+            }
+            subsections.add(section);
+            return this;
+        }
+
+        public int getCount() {
+            int sum = mHeader == null ? 0 : 1;
+            if (subsections != null && subsections.size() != 0) {
+                for (Section sub : subsections) {
+                    sum += sub.getCount();
+                }
+            } else {
+                sum += mItems.size();
+            }
+
+            return sum;
+        }
+
+        public LineItem getItem(int position) {
+            if (mHeader != null && position == start) {
+                return mHeader;
+            }
+
+            if (subsections != null) {
+                for (Section sub : subsections) {
+                    if (sub.contains(position)) {
+                        return sub.getItem(position);
+                    }
+                }
+            }
+
+            return mItems.get(position - start - (mHeader != null ? 1 : 0));
+        }
+
+        private boolean contains(int position) {
+            return start <= position && position <= end;
         }
     }
 }
